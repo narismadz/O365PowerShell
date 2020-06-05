4fd18b2954469ec68a98ce3bad9d9ab02bb4b22c
Narisorn Limpaswadpaisarn <narisorn@outlook.com>
2020-06-05 16:05:17 +0700

    Add files via upload

---
+++ Remove all AAD Guest users.ps1
@@ -0,0 +1,23 @@
+﻿
+# Install Azure AD PowerShell Module 
+# Skip these if you've already installed
+Uninstall-Module AzureAD -AllVersions
+Uninstall-Module AzureADPreview -AllVersions
+Install-Module AzureADPreview
+
+# need to sign in first
+Connect-AzureAD 
+
+
+# Export all guest users
+$GuestUserList = Get-AzureADUser | where {$_.UserType -eq 'Guest'} 
+
+
+
+# Delete all guest user
+
+Get-AzureADUser | where {$_.UserType -eq 'Guest'} | Remove-AzureADUser
+
+# Check the results
+$GuestUserList
+
