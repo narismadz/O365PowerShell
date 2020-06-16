3d57ef82c7e57e02e57bd11fae28ea322f49dbb2
Narisorn Limpaswadpaisarn <narisorn@outlook.com>
2020-06-16 11:19:06 +0700

    Add files via upload

---
+++ See Dynamic Distribution Group Member.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module -ExchangeOnlineManagement
+
+# Sign in
+Connect-ExchangeOnline
+
+# Ex Dynamic group name "test dynamic"
+$FTE = Get-DynamicDistributionGroup "test dynamic"
+
+
+# See it's member
+Get-Recipient -RecipientPreviewFilter $FTE.RecipientFilter -OrganizationalUnit $FTE.RecipientContainer
+
+# see group properties
+$FTE | Format-List
