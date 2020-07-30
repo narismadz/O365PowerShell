9cdbcacf0fff153e20d1a27adf9a72c740ba4000
Narisorn Limpaswadpaisarn <narisorn@outlook.com>
2020-07-30 13:15:28 +0700

    Add files via upload

---
+++ Count or Lists AAD Objects.ps1
@@ -0,0 +1,36 @@
+﻿#Install NuGet offline (if you cann't install AzureAD module due to NuGet)
+# Open PS as Admin
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider -Name NuGet
+
+
+# Install Azure AD PowerShell Module 
+# Skip these if you've already installed
+Uninstall-Module AzureAD -AllVersions
+Uninstall-Module AzureADPreview -AllVersions
+Install-Module AzureADPreview
+
+
+# Connect Azure AD
+Connect-AzureAD
+
+
+# count of Azure AD Users
+(Get-AzureADUser -all $true | Where {$_.DirSyncEnabled -eq $true}).Count
+
+
+# List all Azure users
+Get-AzureADUser -all $true | Where {$_.DirSyncEnabled -eq $true}
+
+
+#count of cloud only AAD users
+(Get-AzureADUser -all $true | Where {$_.DirSyncEnabled -eq $null}).Count
+
+
+# List of cloud only AAD users
+Get-AzureADUser -all $true | Where {$_.DirSyncEnabled -eq $null}
+
+# Count license and quotas
+Get-AzureADSubscribedSku | select SkuPartNumber,ConsumedUnits -ExpandProperty PrepaidUnits
+
