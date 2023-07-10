30db8ad87a519f5021cef7bc5cd709a544fde422
Narisorn Limpaswadpaisarn <narisorn@outlook.com>
2023-07-10 21:09:20 +0700

    Add files via upload

---
+++ MSGraph-PowerShell/AddLicenseToEachUserInBulkFromCSV.ps1
@@ -0,0 +1,44 @@
+﻿# Input your csv file path here
+$csvPath = "C:\Import-User-Password.csv"
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+# Install-Module -Name Microsoft.Graph #hide this if already installed
+
+
+Connect-MgGraph -Scopes User.ReadWrite.All, Organization.Read.All
+
+
+<# csv fields includes
+- UserPrincipalName
+- FirstName
+- LastName
+- DisplayName
+- AccountSKUId *** no need to put domain
+- UsageLocation
+- Password
+#>
+
+
+Import-Csv -Path $csvPath | foreach {
+
+# Get SKU ID
+$Sku = Get-MgSubscribedSku -All | `
+Where SkuPartNumber -eq $_.AccountSkuId
+$addLicenses = @(
+  @{SkuId = $Sku.SkuId}
+  )
+# Get user based on UserPrincipalName in csv 
+$user = Get-MgUser -UserId $_.UserPrincipalName 
+
+# Assign license
+Set-MgUserLicense -UserId $user.Id  `
+-AddLicenses $addLicenses `
+-RemoveLicenses @()
+
+}
+
+
+
+
+
