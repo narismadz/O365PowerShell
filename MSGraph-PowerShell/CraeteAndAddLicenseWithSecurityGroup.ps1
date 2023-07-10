efefc8e26ac062ee27546999a46fcb649cd343e7
Narisorn Limpaswadpaisarn <narisorn@outlook.com>
2023-07-10 21:44:00 +0700

    Add files via upload

---
+++ MSGraph-PowerShell/CraeteAndAddLicenseWithSecurityGroup.ps1
@@ -0,0 +1,45 @@
+﻿# Input your csv file path here
+$csvPath = "C:\Import-User-Password.csv"
+# Name your group here
+$SGGroupName = "M365-A5-users"
+# specify license
+$License = "M365EDU_A5_FACULTY"
+
+
+Connect-MgGraph -Scopes User.ReadWrite.All, Organization.Read.All, Group.ReadWrite.All
+Import-Module Microsoft.Graph.Groups
+
+# Create Security Group
+
+$SGGroupId = New-MgGroup -DisplayName $SGGroupName `
+-MailEnabled:$False `
+-MailNickName $SGGroupName -SecurityEnabled
+
+# Add member from csv to Group
+
+Import-Csv -Path $csvPath | foreach {
+$DirectoryObjId = Get-MgUser -UserId $_.UserPrincipalName
+New-MgGroupMember -GroupId $SGGroupId.Id -DirectoryObjectId $DirectoryObjId.Id
+}
+
+# Assign license to Group
+
+$Sku = Get-MgSubscribedSku -All | `
+Where SkuPartNumber -eq $License
+
+$params = @{
+    AddLicenses = @(
+        @{
+            # Remove the DisabledPlans key as we don't need to disable any service plans
+            # Specify the SkuId of the license you want to assign
+            SkuId = $Sku.SkuId
+        }
+    )
+    # Keep the RemoveLicenses key empty as we don't need to remove any licenses
+    RemoveLicenses = @(
+    )
+}
+
+Set-MgGroupLicense -GroupId $SGGroupId.Id -BodyParameter $params
+
+
