0a61b94ee653330ece9185eaa5312aefe001b95f
Narisorn Limpaswadpaisarn <narisorn@outlook.com>
2024-08-29 17:07:58 +0800

    Update PBIDesktopDownloadInstall.ps1

--- PBIDesktopDownloadInstall.ps1
+++ PBIDesktopDownloadInstall.ps1
@@ -1,5 +1,5 @@
 # Download Power BI Desktop
-Invoke-WebRequest -Uri "https://download.microsoft.com/download/0/1/2/01234567-89AB-CDEF-0123-456789ABCDEF/PowerBIDesktopSetup_x64.exe" -OutFile "C:\Temp\PowerBIDesktopSetup_x64.exe"
+Invoke-WebRequest -Uri "https://www.microsoft.com/en-us/download/details.aspx?id=58494" -OutFile "C:\Temp\PowerBIDesktopSetup_x64.exe"
 
 # Install Power BI Desktop silently
 Start-Process -FilePath "C:\Temp\PowerBIDesktopSetup_x64.exe" -ArgumentList "/quiet" -Wait
